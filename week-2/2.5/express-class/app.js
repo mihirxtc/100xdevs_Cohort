79e0e5af22d2917295324277112c06069deae0e8
mihirxtc <mihirxtc@gmail.com>
2024-01-15 02:36:05 +0530

    3.1 to be continued

--- week-2/2.5/express-class/app.js
+++ week-2/2.5/express-class/app.js
@@ -1,3 +1,4 @@
+ear
 const express = require("express");
 const app = express();
 
---
+++ week-3/3.1/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/health-checkup", (req, res) => {
+    const kidneys = req.body.kidneys;
+    const kidneyLength = kidneys.length;
+
+    res.send("you have " + kidneyLength + " kindeys")
+});
+
+app.listen(5000)
