48069aef043848a6a5db9fb9a845c12c2b9aaefb
mihirxtc <mihirxtc@gmail.com>
2024-04-20 18:00:11 +0530

    week-7 completed

---
+++ week-7/selector-family/src/atoms.ts
@@ -0,0 +1,26 @@
+import { atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
+
+export const todosAtomFamily = atomFamily({
+  key: 'todosAtomFamily',
+  default: selectorFamily({
+    key: "todoSelectorFamily",
+    get: (id) => async ({get}) => {
+      const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+      return res.data.todo;
+    },
+  })
+});
+
+// export const todosAtomFamily = atomFamily({
+//   key: "todosAtomFamily",
+//   default: selectorFamily({
+//     key: "todosSelectorFamily",
+//     get: function(id) {
+//       return async function ({get}) {
+//         const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`);
+//         return res.data.todo;
+//       }
+//     }
+//   })
+// })
